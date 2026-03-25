af9b8efd73e822d5f2e69b9f0d25270f032d8310
fahmi-nugroho <fahminugroho23@gmail.com>
2026-03-25 11:29:45 +0700

    feat: Logging - Transport File

---
+++ NodeJS Logging/test/logger-file-transport.test.js
@@ -0,0 +1,12 @@
+import winston from 'winston';
+
+test('create new logger file transport', () => {
+  const logger = winston.createLogger({
+    transports: [
+      new winston.transports.File({ filename: 'application.log' }),
+      new winston.transports.Console()
+    ]
+  })
+
+  logger.info('Hello, Winston!');
+})
